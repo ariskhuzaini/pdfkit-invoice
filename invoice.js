b2ba385abc5b44aeac47c22fd4a5af1e6e665fb8
ariskhuzaini <ariskhuzaini@gmail.com>
2023-09-07 08:31:19 +0700

    fix: fix

--- invoice.js
+++ invoice.js
@@ -6,7 +6,7 @@ const PDFDocument = require("pdfkit");
 // const invoice = JSON.parse(fs.readFileSync("invoice.json"));
 
 function createInvoice(invoice) {
-  let doc = new PDFDocument({ size: "A4", margin: 50 });
+  let doc = new PDFDocument({ size: "A4", margin: 40 });
 
   generateHeader(doc);
   generateCustomerInformation(doc, invoice);
@@ -34,11 +34,11 @@ function generateCustomerInformation(doc, invoice) {
   doc
     .fillColor("#444444")
     .fontSize(20)
-    .text("Invoice", 50, 160);
+    .text("Invoice", 50, 150);
 
-  generateHr(doc, 185);
+  generateHr(doc, 175);
 
-  const customerInformationTop = 200;
+  const customerInformationTop = 190;
 
   doc
     .fontSize(10)
@@ -68,12 +68,100 @@ function generateCustomerInformation(doc, invoice) {
     )
     .moveDown();
 
-  generateHr(doc, 252);
+  generateHr(doc, 242);
 }
+
+// non responsive
+// function generateInvoiceTable(doc, invoice) {
+//   let i;
+//   const invoiceTableTop = 300;
+
+//   doc.font("Helvetica-Bold");
+//   generateTableRow(
+//     doc,
+//     invoiceTableTop,
+//     "No.",
+//     "Name",
+//     "Code",
+//     "Price",
+//     "Quantity",
+//     "Total Price"
+//   );
+//   generateHr(doc, invoiceTableTop + 20);
+//   doc
+//     .font("Helvetica");
+
+//   for (i = 0; i < invoice.items.length; i++) {
+//     const item = invoice.items[i];
+//     const position = invoiceTableTop + (i + 1) * 30;
+//     generateTableRow(
+//       doc,
+//       position,
+//       i + 1 + ".", // Number the items from 1 to n
+//       item.product,
+//       item.code,
+//       formatCurrency(item.price),
+//       item.quantity,
+//       formatCurrency(item.totalPrice)
+//     );
+
+//     generateHr(doc, position + 20);
+//   }
+
+//   const subtotalPosition = invoiceTableTop + (i + 1) * 30;
+//   generateTableRow(
+//     doc,
+//     subtotalPosition,
+//     "",
+//     "",
+//     "",
+//     "Subtotal",
+//     "",
+//     formatCurrency(invoice.subtotal)
+//   );
+
+//   const paidToDatePosition = subtotalPosition + 20;
+//   generateTableRow(
+//     doc,
+//     paidToDatePosition,
+//     "Payment is due within 15 days.",
+//     "",
+//     "",
+//     "Paid To Date",
+//     "",
+//     formatCurrency(invoice.paid)
+//   );
+
+//   const duePosition = paidToDatePosition + 25;
+//   doc.font("Helvetica-Bold");
+//   generateTableRow(
+//     doc,
+//     duePosition,
+//     "Thank you for your business.",
+//     "",
+//     "",
+//     "Balance Due",
+//     "",
+//     formatCurrency(invoice.subtotal - invoice.paid)
+//   );
+// }
+
+
+// function generateFooter(doc) {
+//   doc
+//     .fontSize(10)
+//     .text(
+//       "Payment is due within 15 days. Thank you for your business.",
+//       50,
+//       720,
+//       { align: "center", width: 500 }
+//     );
+// }
 
 function generateInvoiceTable(doc, invoice) {
   let i;
-  const invoiceTableTop = 330;
+  let invoiceTableTop = 300; // Initial position
+  const maxHeight = doc.page.height - doc.page.margins.bottom; // Updated variable name
 
   doc.font("Helvetica-Bold");
   generateTableRow(
@@ -91,11 +179,31 @@ function generateInvoiceTable(doc, invoice) {
 
   for (i = 0; i < invoice.items.length; i++) {
     const item = invoice.items[i];
-    const position = invoiceTableTop + (i + 1) * 30;
+    let position = invoiceTableTop + (i + 1) * 30; // Declare as 'let'
+
+    // Check if a new page is needed
+    if (position > maxHeight - 50) { // Updated variable name
+      doc.addPage(); // Start a new page
+      invoiceTableTop = 50; // Reset the table position
+      generateTableRow(
+        doc,
+        invoiceTableTop,
+        "No.",
+        "Name",
+        "Code",
+        "Price",
+        "Quantity",
+        "Total Price"
+      );
+      generateHr(doc, invoiceTableTop + 20);
+      doc.font("Helvetica");
+      position = invoiceTableTop + 30; // Update the current position
+    }
+
     generateTableRow(
       doc,
       position,
-      i + 1 + ".", // Number the items from 1 to n
+      i + 1 + ".",
       item.product,
       item.code,
       formatCurrency(item.price),
@@ -106,7 +214,7 @@ function generateInvoiceTable(doc, invoice) {
     generateHr(doc, position + 20);
   }
 
-  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
+const subtotalPosition = invoiceTableTop + (i + 1) * 30;
   generateTableRow(
     doc,
     subtotalPosition,
@@ -122,7 +230,7 @@ function generateInvoiceTable(doc, invoice) {
   generateTableRow(
     doc,
     paidToDatePosition,
-    "",
+    "Payment is due within 15 days.",
     "",
     "",
     "Paid To Date",
@@ -135,24 +243,34 @@ function generateInvoiceTable(doc, invoice) {
   generateTableRow(
     doc,
     duePosition,
-    "",
+    "Thank you for your business.",
     "",
     "",
     "Balance Due",
     "",
     formatCurrency(invoice.subtotal - invoice.paid)
   );
-  doc.font("Helvetica");
 }
 
+
 function generateFooter(doc) {
+  const pageHeight = doc.page.height - doc.page.margins.bottom;
   doc
+    .font("Helvetica-Bold")
     .fontSize(10)
     .text(
-      "Payment is due within 15 days. Thank you for your business.",
+      "Terms and Conditions",
       50,
-      780,
-      { align: "center", width: 500 }
+      pageHeight - 60
+    )
+    .font("Helvetica")
+    .text("1. The invoice must be paid in full no later than the due date specified on the invoice.",
+      50,
+      pageHeight - 40
+    )
+    .text("2. This invoice is considered valid only if it is paid in full by the due date.",
+    )
+    .text("3. Payments received after the due date may not be accepted or may be subject to late payment fees.",
     );
 }
 
@@ -171,17 +289,18 @@ function generateTableRow(
     .text(no, 50, y)
     .text(code, 90, y)
     .text(name, 260, y)
-    .text(price, 350, y, { width: 90})
+    .text(price, 340, y, { width: 90})
     .text(quantity, 420, y, { width: 50, align: 'center'})
     .text(totalPrice, 0, y, { align: "right" });
 }
 
 function generateHr(doc, y) {
+  const docWidth = doc.page.width - doc.page.margins.left - doc.page.margins.right;
   doc
     .strokeColor("#aaaaaa")
     .lineWidth(1)
-    .moveTo(50, y)
-    .lineTo(550, y)
+    .moveTo(doc.page.margins.left, y)
+    .lineTo(doc.page.margins.left + docWidth, y)
     .stroke();
 }
 
